3834699a11eb57371acfa5ce7cd9af1a7e6f330a
Gabriel Borges <gabrielfr.borges@gmail.com>
2021-11-26 21:14:16 -0300

    fix: point to back-end at heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://13f3-2804-431-cfef-102b-f377-a629-8065-50b2.ngrok.io',
+  baseURL: 'https://password-safe-fatec.herokuapp.com/api',
 });
 
 export default api;
